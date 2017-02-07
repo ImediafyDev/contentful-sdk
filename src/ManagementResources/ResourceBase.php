0bebdd27ed40c65ac9bf85cdabf36e58226cbc06
Phillip Hartin <mail@philliphartin.com>
2017-02-07 17:51:38 +1100

    Updating put method to take entry content type as an argument and set headers correctly.

--- src/ManagementResources/ResourceBase.php
+++ src/ManagementResources/ResourceBase.php
@@ -2,51 +2,64 @@
 
 namespace Incraigulous\ContentfulSDK\ManagementResources;
 
-
-abstract class ResourceBase extends \Incraigulous\ContentfulSDK\Resources\ResourceBase {
+abstract class ResourceBase extends \Incraigulous\ContentfulSDK\Resources\ResourceBase
+{
     protected $clientClassName = 'Incraigulous\ContentfulSDK\ManagementClient';
 
     /**
      * Make a post request.
+     *
      * @param $payload
+     *
      * @return mixed
      */
-    function post($payload)
+    public function post($payload)
     {
         $this->requestDecorator->setPayload($payload);
         $result = $this->client->post($this->requestDecorator->makeResource(), $this->requestDecorator->makePayload(), $this->requestDecorator->makeHeaders());
         $this->refresh();
+
         return $result;
     }
 
-    /**
-     * Make a put request.
-     * @param $id
-     * @param $payload
-     * @return mixed
-     */
-    function put($id, $payload)
-    {
-        $this->requestDecorator->setId($id);
-        $this->requestDecorator->setPayload($payload);
-        if ((is_array($payload)) && (!empty($payload['sys']['version']))) {
-            $this->requestDecorator->addHeader('X-Contentful-Version', $payload['sys']['version']);
-        }
-        $result = $this->client->put($this->requestDecorator->makeResource(), $this->requestDecorator->makePayload(), $this->requestDecorator->makeHeaders());
-        $this->refresh();
-        return $result;
-    }
+     /**
+      * Make a put request.
+      *
+      * @param $id
+      * @param $payload
+      *
+      * @return mixed
+      */
+     public function put($id, $payload, $contentType = null)
+     {
+         $this->requestDecorator->setId($id);
+         $this->requestDecorator->setPayload($payload);
+
+         if (isset($contentType)) {
+             $this->requestDecorator->addHeader('X-Contentful-Content-Type', $contentType);
+         }
+         if ((is_array($payload)) && (!empty($payload['sys']['version']))) {
+             $this->requestDecorator->addHeader('X-Contentful-Version', $payload['sys']['version']);
+         }
+         $result = $this->client->put($this->requestDecorator->makeResource(), $this->requestDecorator->makePayload(), $this->requestDecorator->makeHeaders());
+         $this->refresh();
+
+         return $result;
+     }
 
     /**
      * Make a delete request.
+     *
      * @param $id
+     *
      * @return mixed
      */
-    function delete($id)
+    public function delete($id)
     {
         $this->requestDecorator->setId($id);
         $result = $this->client->delete($this->requestDecorator->makeResource(), $this->requestDecorator->makeHeaders());
         $this->refresh();
+
         return $result;
     }
 }
